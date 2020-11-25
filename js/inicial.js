914a1b2c61b0653e5085d65d127fb37348790e2e
caio silva barbara <66045981+caioSBarbara@users.noreply.github.com>
2020-11-25 14:26:59 -0300

    Add files via upload

--- js/inicial.js
+++ js/inicial.js
@@ -1,23 +1,47 @@
 function mouseEmCima(imagem) {
-
     imagem.src = "../../imagens/cadastro2.png";
-
 }
 
 function mouseFora(imagem) {
-
     imagem.src = "../../imagens/icone-cadastro.png";
-
 }
 /*--------------------------*/
 function mouseEmCima2(imagem) {
-
     imagem.src = "../../imagens/icone-compras2.png";
-
 }
 
 function mouseFora2(imagem) {
-
     imagem.src = "../../imagens/icone-compras.png";
+}
+/*------------------------------*/
+function mouseEmCima3(imagem) {
+    imagem.src = "../../imagens/icone-login2.png";
+}
 
+function mouseFora3(imagem) {
+    imagem.src = "../../imagens/icone-login.png";
+}
+/*--------------------------------------------*/
+function mouseEmCima4(imagem) {
+    imagem.src = "../../imagens/facebook2.png";
+}
+
+function mouseFora4(imagem) {
+    imagem.src = "../../imagens/facebook.png";
+}
+/*---------------------------------*/
+function mouseEmCima5(imagem) {
+    imagem.src = "../../imagens/instagram2.png";
+}
+
+function mouseFora5(imagem) {
+    imagem.src = "../../imagens/instagram.png";
+}
+/*--------------------------------*/
+function mouseEmCima6(imagem) {
+    imagem.src = "../../imagens/twitter2.png";
+}
+
+function mouseFora6(imagem) {
+    imagem.src = "../../imagens/twitter.png";
 }
